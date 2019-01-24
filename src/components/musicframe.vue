9b3ba474ea433407c517559da602af8a1d325a47
Sophia Choi <krinsoch@ac.obigo.com>
2019-01-24 18:42:23 +0900

    [DEV5-2] audio player with vue.js

---
+++ src/components/musicframe.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="musicframe">
+    <p>{{currentInfo}}</p>
+    <div class="img">
+      <img v-bind:src="currentImage" width="100%" height="100%"/>
+    </div>
+    <div class="btm" id="controls">
+      <audio ref="audio" id="audio">
+        <source v-bind:src="currentSong"/>
+        not supported
+      </audio>
+      <span class="btn fa fa-play fa-4x" v-on:click="play"></span>
+      <span class="btn fa fa-pause fa-4x" v-on:click="pause"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import {bus} from '../main'
+export default {
+  name: 'musicframe',
+  props: {
+    currentSong: {
+      type: String
+    },
+    currentImage: {
+      type: String
+    },
+    currentInfo: {
+      type: String
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    play () {
+      console.log('Clicked Play Button')
+      this.$refs.audio.play()
+    },
+    pause () {
+      console.log('Clicked Pause Button')
+      this.$refs.audio.pause()
+    }
+  },
+  created: function () {
+    bus.$on('reload', () => {
+      this.$refs.audio.load()
+      this.$refs.audio.play()
+    })
+  }
+}
+</script>
+<style lang='scss' scoped>
+.contents {
+  padding:20px;
+  color: white;
+  & > p,
+  & > div{
+    padding:20px;
+  }
+}
+  p {
+    display: block;
+    font-size: 30px;
+    text-align: center;
+    margin: 8px;
+  }
+  .img {
+    height: auto;
+  }
+  img {
+    border: 1px;
+    border-radius: 4px;
+    padding: 10px;
+  }
+  #controls {
+    border: none;
+    color: white;
+    font-size: 1em;
+    position: absolute;
+    top: 280px;
+    width: 400px;
+    height: 100px;
+    text-align: center;
+  }
+  .btn {
+    width: 5rem;
+  }
+</style>
---
+++ src/components/playlist.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="playlist">
+    <div id="secborder" ref="playlist" @click="changeSource">
+      <div class="song" url-value="http://img0.liveinternet.ru/images/attach/b/3/3656/3656927_eminem__mockingbird.mp3" picture-value="https://wallpapercave.com/wp/WlHnNj6.jpg" info-value="Eminem - Mocking Bird">Eminem - Mocking Bird</div>
+      <div class="song" url-value="http://soundbible.com/mp3/Air%20Plane%20Ding-SoundBible.com-496729130.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg" info-value="Linkin Park - Faint">Linkin Park - Faint</div>
+      <div class="song" url-value="http://rm.sina.com.cn/wm/VZ2008072314565310537VK/music/MUSIC0807231507294087.mp3" picture-value="https://i.ytimg.com/vi/oY5BYjsrIHk/maxresdefault.jpg" info-value="Linkin Park - Somewhere I belong">Linkin Park - Somewhere I belong</div>
+      <div class="song" url-value="http://a.tumblr.com/tumblr_mirhiitqaC1roewk4o1_r1.mp3" picture-value="https://i.ytimg.com/vi/vAsztnDeZ1Q/maxresdefault.jpg" info-value="Demi Lovato - Heart Attack">Demi Lovato - Heart Attack</div> 
+      <div class="song" url-value="@/Kalimba.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg"> local - kalimba </div>
+      <div class="song" url-value="@/src/asset/Kalimba.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg"> local - kalimba </div>
+      <div class="song" url-value="C:\Users\krinsoch\Documents\workspace\MyApp\src\asset\Kalimba.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg"> local - kalimba </div>
+      <div class="song" url-value="C:\Users\krinsoch\Documents\workspace\MyApp\src\asset\Kalimba.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg"> local - kalimba </div>
+      <div class="song" url-value="C:\Users\krinsoch\Documents\workspace\MyApp\src\asset\Kalimba.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg"> local - kalimba </div>
+      <div class="song" url-value="C:\Users\krinsoch\Documents\workspace\MyApp\src\asset\Kalimba.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg"> local - kalimba </div>
+      <div class="song" url-value="C:\Users\krinsoch\Documents\workspace\MyApp\src\asset\Kalimba.mp3" picture-value="https://orig00.deviantart.net/65e3/f/2009/335/6/2/linkin_park__faint_by_jimeye.jpg"> local - kalimba </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { bus } from '../main'
+export default {
+  name: 'playlist',
+  props: {
+    currentSong: {
+      type: String
+    },
+    currentImage: {
+      type: String
+    },
+    currentInfo: {
+      type: String
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    changeSource: function (e) {
+      let songValue = e.target.attributes[1].nodeValue
+      let songImage = e.target.attributes[2].nodeValue
+      let songInfo = e.target.attributes[3].value
+      console.log(songValue, songImage, songInfo, 'playlist changSource log')
+      this.$emit('updateData', [songValue, songImage, songInfo])
+      bus.$emit('reload')
+    }
+  }
+}
+</script>
+<style scoped>
+#secborder {
+    border: 5px solid white;
+    border-radius: 15px;
+    margin: 2rem 0.5rem;
+    background: black;
+    height: 300px; /* 20srem; */
+    width: 380px;
+    overflow: scroll;
+}
+.song {
+    width: 100%;
+    height: auto;
+    padding: 0.5em;
+    border-bottom: 0.5px solid #172951;
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-align: center;
+    color: white;
+    cursor: pointer;
+}
+</style>
