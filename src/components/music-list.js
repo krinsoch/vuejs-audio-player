8665f06e3c75cdcefb46cd4ce08bf5935b834384
krinsoch <sophia.choi@obigo.com>
2019-01-31 19:01:02 +0900

    [DEV5-6] apply obg js ui and load local file

---
+++ src/components/music-list.js
@@ -0,0 +1,47 @@
+export const MUSIC = [
+  {
+    url: 'http://img0.liveinternet.ru/images/attach/b/3/3656/3656927_eminem__mockingbird.mp3',
+    image: 'https://wallpapercave.com/wp/WlHnNj6.jpg',
+    info: 'Eminem - Mocking Bird'
+  },
+  {
+    url: 'http://soundbible.com/mp3/Air%20Plane%20Ding-SoundBible.com-496729130.mp3',
+    image: 'https://wallpapercave.com/wp/WlHnNj6.jpg',
+    info: 'Linkin Park - Faint'
+  },
+  {
+    url: 'http://rm.sina.com.cn/wm/VZ2008072314565310537VK/music/MUSIC0807231507294087.mp3',
+    image: 'https://i.ytimg.com/vi/oY5BYjsrIHk/maxresdefault.jpg',
+    info: 'Linkin Park - Somewhere I belong'
+  },
+  {
+    url: 'http://a.tumblr.com/tumblr_mirhiitqaC1roewk4o1_r1.mp3',
+    image: 'https://i.ytimg.com/vi/vAsztnDeZ1Q/maxresdefault.jpg',
+    info: 'Demi Lovato - Heart Attack'
+  },
+  {
+    url: 'resource/Kalimba.mp3',
+    image: 'resource/001.jpg',
+    info: 'local - Kalimba'
+  },
+  {
+    url: 'resource/Maid with the Flaxen Hair.mp3',
+    image: 'https://i.ytimg.com/vi/oY5BYjsrIHk/maxresdefault.jpg',
+    info: 'local - Maid with the Flaxen Hair'
+  },
+  {
+    url: 'resource/Sleep Away.mp3',
+    image: 'https://i.ytimg.com/vi/oY5BYjsrIHk/maxresdefault.jpg',
+    info: 'Local - Sleep Away'
+  },
+  {
+    url: 'http://rm.sina.com.cn/wm/VZ2008072314565310537VK/music/MUSIC0807231507294087.mp3',
+    image: 'https://i.ytimg.com/vi/oY5BYjsrIHk/maxresdefault.jpg',
+    info: 'Linkin Park - Somewhere I belong'
+  },
+  {
+    url: 'http://rm.sina.com.cn/wm/VZ2008072314565310537VK/music/MUSIC0807231507294087.mp3',
+    image: 'https://i.ytimg.com/vi/oY5BYjsrIHk/maxresdefault.jpg',
+    info: 'Linkin Park - Somewhere I belong'
+  }
+]
